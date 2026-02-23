e816655a8ec2519ed937567c474f1925916161c3
Natalia Panchuk <Foreverdanda@ukr.net>
2026-02-23 09:48:53 +0200

    solution2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 
 import './App.scss';
-import { MovieList } from './components/MovieList/MovieList';
+import { MovieList } from './components/MovieList';
 import movies from './api/movies.json';
 import { Sidebar } from './components/Sidebar';
 
